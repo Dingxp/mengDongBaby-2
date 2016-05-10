4c892ade8b8ec4fb9be7432bc55950b28ee9ba16
丁旭朋 <dingxupeng@mac-mini.local>
2016-05-10 16:41:17 +0800

    Initial Commit

---
+++ BabyLiFang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyLiFang
+//
+//  Created by 丁旭朋 on 16/5/10.
+//  Copyright © 2016年 丁旭朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
